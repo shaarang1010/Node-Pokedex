a9a6a42f88293b86f314f50275225ed372e35951
Shaarang Tanpure <shaarang.1910@gmail.com>
2022-02-09 23:55:28 +1100

    add getters

--- routes/get/index.js
+++ routes/get/index.js
@@ -2,4 +2,8 @@ const router = require("express").Router();
 
 router.get("/", (req, res) => {});
 
+router.get("/pokemon/:pokemonid", (req, res) => {});
+
+router.get("/pokemon", (req, res) => {});
+
 module.exports = router;
